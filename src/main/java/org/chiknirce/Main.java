ce4383a9b3e6cfca464ebdc8daabf46959697f75
ian <chiknrice@gmail.com>
2016-11-07 14:59:19 +0800

    initial commit

---
+++ src/main/java/org/chiknirce/Main.java
@@ -0,0 +1,12 @@
+package org.chiknirce;
+
+/**
+ * @author <a href="mailto:chiknrice@gmail.com">Ian Bondoc</a>
+ */
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+
+}
